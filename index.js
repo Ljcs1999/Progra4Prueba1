465e842562094a5df7d6795d1c21e22977bb82e5
Ljcs1999 <Ljcs_1999@hotmail.com>
2022-10-28 18:14:20 -0700

    Desarrollo

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const router = require('./router.js')
 require('dotenv').config()
 
 
-
 const server = http.createServer((peticion,respuesta)=>{
     console.log(peticion.url);
     switch(peticion.url){
-        case '/':
-            router.index(peticion,respuesta)
+        case '/1':
+            //router.index(peticion,respuesta)
+            
+            let multiplicacion = 1*yargs.argv.n2
+            respuesta.writeHead(200,{'Content-Type':'application/json'});
+            respuesta.write(`La multiplicacion de los numeros 1 * ${n2} es: ${multiplicacion}`);
             break
         case '/empleado':
             router.empleado(peticion,respuesta) 
@@ -20,7 +23,7 @@ const server = http.createServer((peticion,respuesta)=>{
         default:
             respuesta.writeHead(404,{'Content-Type':'text/plain'});
             respuesta.write('Pagina no disponible');
-            break       
+            break
     }
     respuesta.end();
 })
--- router.js
+++ router.js
@@ -1,16 +1,6 @@
-const yargs = require('yargs').option('n1',{alias:'n1',type:'number'}).option('n2',{alias:'n2',type:'number'}).check((argv,options)=>{
-    if(argv.n1<0 || argv.n2<0){
-        throw 'El numero debe ser mayor que cero'
-    }else{
-        return true
-    }
-})
-
-let multiplicacion = yargs.argv.n1*yargs.argv.n2
-
 
 function index(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.writeHead(200,{'Content-Type':'application/json'});
     res.write(`La multiplicacion de los numeros ${n1} y ${n2} es: ${multiplicacion}`);
 }
 
