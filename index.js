04a751b0b9dc7a7caf735b5640b3f5b7dbfc4b37
Ljcs1999 <Ljcs_1999@hotmail.com>
2022-10-28 18:27:20 -0700

    Creacion del case 2 al 5

--- index.js
+++ index.js
@@ -10,24 +10,70 @@ const server = http.createServer((peticion,respuesta)=>{
             //router.index(peticion,respuesta)
 
             const yargs = require('yargs').option('n2',{alias:'n2',type:'number'}).check((argv,options)=>{
-                if(argv.n1<0 || argv.n2<0){
+                if(argv.n2<0){
                     throw 'El numero debe ser mayor que cero'
                 }else{
                     return true
                 }
             })
 
-            let multiplicacion = 5*yargs.argv.n2
+            let multiplicacion = 1*yargs.argv.n2
             respuesta.writeHead(200,{'Content-Type':'text/plain'});
             respuesta.write(`La multiplicacion es: ${multiplicacion}`);
             break
-        case '/empleado':
-            router.empleado(peticion,respuesta) 
-            break
-        case '/about':
+        case '/2':
+            const yargs1 = require('yargs').option('n2',{alias:'n2',type:'number'}).check((argv,options)=>{
+                if(argv.n2<0){
+                    throw 'El numero debe ser mayor que cero'
+                }else{
+                    return true
+                }
+            })
+
+            let multiplicacion2 = 2*yargs1.argv.n2
             respuesta.writeHead(200,{'Content-Type':'text/plain'});
-            respuesta.write('Sitio desarrollado por Levi');
+            respuesta.write(`La multiplicacion es: ${multiplicacion2}`);
             break
+        case '/3':
+            const yargs2 = require('yargs').option('n2',{alias:'n2',type:'number'}).check((argv,options)=>{
+                if(argv.n2<0){
+                    throw 'El numero debe ser mayor que cero'
+                }else{
+                    return true
+                }
+            })
+
+            let multiplicacion3 = 3*yargs2.argv.n2
+            respuesta.writeHead(200,{'Content-Type':'text/plain'});
+            respuesta.write(`La multiplicacion es: ${multiplicacion3}`);
+            break
+        
+        case '/4':
+            const yargs3 = require('yargs').option('n2',{alias:'n2',type:'number'}).check((argv,options)=>{
+                if(argv.n2<0){
+                    throw 'El numero debe ser mayor que cero'
+                }else{
+                    return true
+                }
+            })
+
+            let multiplicacion4 = 4*yargs3.argv.n2
+            respuesta.writeHead(200,{'Content-Type':'text/plain'});
+            respuesta.write(`La multiplicacion es: ${multiplicacion4}`);
+            break
+        case '/5':
+                const yargs4 = require('yargs').option('n2',{alias:'n2',type:'number'}).check((argv,options)=>{
+                    if(argv.n2<0){
+                        throw 'El numero debe ser mayor que cero'
+                    }else{
+                        return true
+                    }
+                })
+    
+                let multiplicacion5 = 5*yargs4.argv.n2
+                respuesta.writeHead(200,{'Content-Type':'text/plain'});
+                respuesta.write(`La multiplicacion es: ${multiplicacion5}`);
+                break    
         default:
             respuesta.writeHead(404,{'Content-Type':'text/plain'});
             respuesta.write('Pagina no disponible');
